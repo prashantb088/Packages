948121e2e689506c71ea4803a991ddb1ea54903a
Prashant Bansal <109963029+prashantb088@users.noreply.github.com>
2022-08-08 14:25:18 +0530

    Update and rename Animal.txt to Animal.java

---
+++ Animal.java
@@ -0,0 +1,14 @@
+/*
+	Program to create an package by an example of an animal
+	by Prashant Bansal
+*/
+
+
+package animals;
+
+interface Animal {
+   
+	public void eat();
+    
+	public void travel();
+}
